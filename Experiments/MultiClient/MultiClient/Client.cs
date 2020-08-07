9ad67806e7224259f6564091330f8d8c384d6f79
iiKurt <ikurt018@gmail.com>
2020-08-07 14:02:45 +1200

    Testing out udpClient via an experiment

---
+++ Experiments/MultiClient/MultiClient/Client.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MultiClient
+{
+    public abstract class Client
+    {
+        public string Name { get; set; }
+    }
+
+    public class LocalClient : Client
+    {
+        public IPAddress IP { get; private set; }
+		public IPEndPoint EndPoint; // cannot be property or Socket receiving doesn't like it
+
+		public LocalClient(ushort port)
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            IP = IPAddress.Parse("127.0.0.1"); // in case we can't find an IP
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP = ip;
+                    break;
+                }
+            }
+
+            EndPoint = new IPEndPoint(IP, port);
+        }
+    }
+
+	public class RemoteClient : Client
+	{
+		private UdpClient client;
+		private LocalClient binding;
+
+		public event EventHandler<NewMessageEventArgs> NewMessage;
+        public event EventHandler<EventArgs> Leave;
+
+        public RemoteClient(LocalClient binding, IPAddress ip, ushort port = 3012, string name = "Other")
+		{
+			this.Name = name;
+			this.binding = binding;
+
+			client = new UdpClient(port);
+			
+			client.BeginReceive(new AsyncCallback(MessageCallBack), null);
+		}
+
+		protected virtual void OnNewMessage(NewMessageEventArgs e)
+		{
+			EventHandler<NewMessageEventArgs> handler = NewMessage;
+			handler?.Invoke(this, e);
+		}
+
+		private void MessageCallBack(IAsyncResult result)
+		{
+			byte[] receivedData = client.EndReceive(result, ref binding.EndPoint);
+			string message = Name + ": " + Encoding.ASCII.GetString(receivedData);
+				
+			NewMessageEventArgs args = new NewMessageEventArgs();
+			args.message = message;
+			NewMessage(this, args);
+
+			client.BeginReceive(new AsyncCallback(MessageCallBack), null);
+		}
+
+		public void Send(string message)
+		{
+			byte[] msg = Encoding.ASCII.GetBytes(message);
+
+			client.Send(msg, msg.Length);
+		}
+	}
+
+	public class NewMessageEventArgs : EventArgs
+	{
+		public string message;
+	}
+}
---
+++ Experiments/MultiClient/MultiClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace MultiClient
+{
+	class Program
+	{
+		const ushort port = 3012;
+
+		static LocalClient self = new LocalClient(port);
+		static List<RemoteClient> clients = new List<RemoteClient>();
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Your IP: " + self.IP.ToString());
+			Console.WriteLine();
+
+			Console.WriteLine("Enter 1st client IP: ");
+			IPAddress ip1 = IPAddress.Parse(Console.ReadLine());
+			clients.Add(new RemoteClient(self, ip1, port, "1st"));
+
+			Console.WriteLine("Enter 2nd client IP: ");
+			IPAddress ip2 = IPAddress.Parse(Console.ReadLine());
+			clients.Add(new RemoteClient(self, ip2, port, "2nd"));
+
+			Console.WriteLine("Listening for messages from those clients...");
+
+			Console.WriteLine("Enter a message to send to both of those clients: ");
+			string message = Console.ReadLine();
+			foreach (RemoteClient client in clients)
+			{
+				client.Send(message);
+			}
+		}
+	}
+}
